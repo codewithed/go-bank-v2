ef92b9a680594e824c74f0f5f2cd590f00feaa1f
Edmund Debrah <edmundking05@gmail.com>
2023-07-31 16:50:16 +0000

    create package util

---
+++ util/random.go
@@ -0,0 +1,43 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(7)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "GBP", "CAD"}
+	k := len(currencies)
+	return currencies[rand.Intn(k)]
+}
+
+func RandomBalance() int64 {
+	return RandomInt(100, 2000)
+}
