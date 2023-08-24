a6a6431064148fb0894f0cd5c2959e7d8744bd17
Edmund Debrah <edmundking05@gmail.com>
2023-08-24 13:19:17 +0000

    update migration schema

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,3 +1,12 @@
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "password_hash" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar NOT NULL UNIQUE,
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00',
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
 CREATE TABLE "accounts" (
   "id" bigserial PRIMARY KEY,
   "owner" varchar NOT NULL,
@@ -35,8 +44,10 @@ COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
 
 COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
 
-ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id") ON DELETE CASCADE;
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id") ON DELETE CASCADE;
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id") ON DELETE CASCADE;
+ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
-
-ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+-- CREATE UNIQUE INDEX ON "accounts" ("owner", "currency")
+ALTER TABLE "accounts" ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner", "currency");
